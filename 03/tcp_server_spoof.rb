dc20ab010e5980f032d9d926c62e695362e20b96
Alexander Timmermann <alex@modprobe.io>
2016-05-25 00:53:44 +0200

    fix typo in spoof server and remove useless assignment

--- 03/tcp_server_spoof.rb
+++ 03/tcp_server_spoof.rb
@@ -1,9 +1,9 @@
 require 'socket'
 
-server = TPServer.new('127.0.1.2', 1337)
+server = TCPServer.new('127.0.1.2', 1337)
 loop do
     client = server.accept # Wait for Client to connect
-    input = client.gets
+    client.gets
     client.puts 'SERIAL_VALID=1'
     client.close
 end
